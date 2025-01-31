ae8ae1f644116128b4b8ffd019390b5a186de3e9
Islam M. Tayeb <islam.tayeb@duke.edu>
2025-01-31 02:17:54 -0500

    Update page.tsx

--- code/app/page.tsx
+++ code/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { motion } from "framer-motion"
 import Hero from "../components/Hero"
 import Experience from "../components/Experience"
 import Projects from "../components/Projects"
