85beda8ddb683421a57592ceaf71f46954c5daea
Islam M. Tayeb <islam.tayeb@duke.edu>
2025-01-31 02:19:32 -0500

    test

--- code/hooks/use-toast.ts
+++ code/hooks/use-toast.ts
@@ -159,7 +159,7 @@ function toast({ ...props }: Toast) {
       ...props,
       id,
       open: true,
-      onOpenChange: (open: any) => {
+      onOpenChange: (open) => {
         if (!open) dismiss()
       },
     },
